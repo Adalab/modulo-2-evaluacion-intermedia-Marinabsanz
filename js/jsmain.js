fafd1a98d53793ed8491f6f00a42adc26172dd7a
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-12 08:51:47 +0200

    create structure

---
+++ js/jsmain.js
@@ -0,0 +1,23 @@
+'use strict'
+/* let randomNUmber = Math.random();
+if (randomNUmber <=20)  {
+
+console.log('Has ganado');
+
+} else  {
+    console.log ('prueba otra vez');
+}
+
+for(let i=0; i>10; i=i+1) {
+
+}
+
+randomNUmber = Math.random ();
+if (randomNUmber <=0.5)  {
+
+    console.log('SI');
+    
+    } else  {
+        console.log ('NO');
+    }
+*/
