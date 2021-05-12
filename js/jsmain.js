c470bf9517ee8bec00223b6f27ee8c0e435e1059
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-12 12:54:24 +0200

    repaso branch  para comparar

--- js/jsmain.js
+++ js/jsmain.js
@@ -1,3 +1,19 @@
+
+/*
+PROCESO 1
+1-Enviar mensaje a usuario
+2-Definir el nº random--- ¿cuándo? AL cargar página
+3-coger nº de usuario- al hacer click en el boton
+4- comparaciones y enviar mensaje
+-(esto una vez que ya tengo el valor de la usuaria)
+
+PROCESO 2
+1-contar el número de intentos
+
+
+*/
+/*
+
 'use strict'
 //const randomNumber = Math.random();
 
@@ -35,6 +51,64 @@ function reply () {
 
     } else if (randomNumberEl >=20)
     return `demasiado bajo`; 
-
+----------SI LO USO DEBERIA TENER Q USAR INNER.HTML TB
+SE ME HA QUEDADO FANTASMA EL RETURN
 } 
 
+*/
+
+
+'use strict'
+function getRandomNumber (max) {
+    return Math.ceil (Math.random()* max);
+}
+    const numberRandom= getRandomNumber(100);
+    console.log (numberRandom);
+
+    //me interesa el input y el boton
+
+    const btn= document.querySelector("js-btn");
+    const inputElement = document.querySelector (".js-input");
+    const msj= document.querySelector (".js-msj");
+    const msjCounter= document.querySelector (".js-counter");
+
+const handleMsj = (event)  =>{
+    event.preventDefault();
+    const inputValue = parseInt(inputElement.value)
+    console.log(inputElement.value);
+
+
+    if(inputValue < 1 || inputValue > 100 ) {
+        msj.innerHTML = "el n debe estar entre 1 y 100"
+    }
+
+else if (inputValue < numberRandom){
+    msj.innerHTML = "es muy bajo"
+}
+else if (inputValue > numberRandom){
+    msj.innerHTML = "el nº es muy alto"
+} 
+else {
+    msj.innerHTML= "ganaste"
+}
+
+counter()
+}
+
+
+    
+function  counter(){
+  let cont = 0
+  cont++;
+  msjCounter.innerHTML = `El nº de intentos ${cont}` ;
+    //let calcul = 0;
+   // function calculate() {
+    //return `Nº de intentos: ` + (calcul += 1);
+    }
+function handleClick (event){
+    event.preventDefault();
+     handleMsj();
+      counter();
+}
+
+ btn.addEventListener("click", handleClick);
