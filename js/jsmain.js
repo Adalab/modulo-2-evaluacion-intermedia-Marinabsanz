44da4efc6ada6df01b9b8606524a82547a20d357
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-12 10:28:43 +0200

    prueba

--- js/jsmain.js
+++ js/jsmain.js
@@ -1,9 +1,24 @@
 'use strict'
 //const randomNumber = Math.random();
 
-const randomNumber= document.querySelector ('js-input');
+const randomNumberEl= document.querySelector ('js-input') 
 
 const buttonEl=document.querySelector('js-write');
 const Pist= document.querySelector ('js-pista');
 const Try= document.querySelector ('js-try');
 const ResultUser= document.querySelector ('js-result');
+
+
+function getRandomNumber(max) {
+return Math.ceil(Math.random() * max);
+}
+
+const randomNumber= getRandomNumber(100);
+console.log (randomNumber);
+
+
+let calcul = 0;
+function calculate() {
+return `Nº de intentos: ` + (calcul += 1);
+}
+
