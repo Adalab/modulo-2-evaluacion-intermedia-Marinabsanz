cca7f20d351782da426980c68a06cdc630300c6d
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-25 12:47:30 +0200

    run

--- js/jsmain.js
+++ js/jsmain.js
@@ -1,25 +1,22 @@
 'use strict';
-/*Generando el número random*/
-function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-  }
-​
-  const randomNumber = getRandomNumber(100); 
-  console.log("El número random es", randomNumber);
-/* hasta aquí*/
-​
 const button = document.querySelector(".js-button");
 const clue = document.querySelector ('.js-cl');
 const attemps = document.querySelector ('.js-att');
 const userNumber = document.querySelector ('.js-user_number'); 
-​
+​  
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+  const randomNumber = getRandomNumber(100); 
+  console.log("El número random es", randomNumber);
+
 const compare = () => {
     const userNumber2 = userNumber.value;
     const userNumbervalue = parseInt (userNumber2);
     console.log (userNumbervalue);
     if (userNumbervalue === randomNumber) {
        clue.innerHTML = 'Has ganado'; 
-    }else if (userNumber2 < 0 || userNumber2 > 100 ) {
+    } else if (userNumber2 < 0 || userNumber2 > 100 ) {
         clue.innerHTML = `El número tiene que ser mayor que cero y menor que cien`; 
     } else if (userNumber2 < randomNumber) {
         clue.innerHTML = `El número es mayor que ${userNumber2}`;
